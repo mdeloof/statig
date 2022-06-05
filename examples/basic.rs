30b459ccbe82cc999a31093e4275aceccec0821b
Maxim Deloof <20443795+mdeloof@users.noreply.github.com>
2022-06-05 13:45:53 +0200

    Add comments

--- examples/basic.rs
+++ examples/basic.rs
@@ -14,11 +14,17 @@ struct Blinky {
 
 struct Event;
 
+// The `stateful` trait needs to be implemented on the type that will be
+// the context for the state machine.
 impl Stateful for Blinky {
+    /// The enum that represents the state. This type is derived by the
+    /// `#[state_machine]` macro.
     type State = State;
 
+    /// The input type that will be submitted to the state machine.
     type Input = Event;
 
+    /// The initial state of the state machine.
     const INIT_STATE: State = State::on();
 }
 
@@ -27,12 +33,14 @@ impl Blinky {
     #[state]
     fn on(&mut self, input: &Event) -> Result<State> {
         self.led = false;
+        // Transition to the `off` state.
         Ok(Transition(State::off()))
     }
 
     #[state]
     fn off(&mut self, input: &Event) -> Result<State> {
         self.led = true;
+        // Transition to the `on` state.
         Ok(Transition(State::on()))
     }
 }
