3014f000625816e5504b61f0ec9c0d7d3a8714b2
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-05-22 18:06:39 -0400

    Fix small typo in docs

--- statig/src/lib.rs
+++ statig/src/lib.rs
@@ -464,7 +464,7 @@
 //!
 //! A lot of the implementation details are dealt with by the `#[state_machine]` macro, but it's always valuable to understand what's happening behind the scenes. Furthermore, you'll see that the generated code is actually pretty straight-forward and could easily be written by hand, so if you prefer to avoid using macro's this is totally feasible.
 //!
-//! The goal of `statig` is to represent a hierarchical state machine. Conceptually a hierarchical state machine can be thought of as tree.
+//! The goal of `statig` is to represent a hierarchical state machine. Conceptually a hierarchical state machine can be thought of as a tree.
 //!
 //! ```text
 //!                           ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐             
